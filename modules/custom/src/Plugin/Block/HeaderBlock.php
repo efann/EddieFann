79cc260fcc432021ca31a3954005c403fdd6d738
Eddie Fann <efann@beowurks.com>
2022-09-19 18:10:07 -0500

    Now programmatically use title & slogan

---
+++ modules/custom/src/Plugin/Block/HeaderBlock.php
@@ -0,0 +1,60 @@
+<?php
+
+// From http://valuebound.com/resources/blog/drupal-8-how-to-create-a-custom-block-programatically
+
+namespace Drupal\custom\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\views\Views;
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+
+/**
+ * Provides a 'quotes' block.
+ *
+ * @Block(
+ *   id = "quotes_block",
+ *   admin_label = @Translation("Header Block"),
+ *   category = @Translation("Custom block for displaying the header.")
+ * )
+ */
+class HeaderBlock extends BlockBase
+{
+  //-------------------------------------------------------------------------------------------------
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $lcContent = "";
+
+    $lcContent .= "<div class='title'><a href='/'>\n";
+    // From https://drupal.stackexchange.com/questions/187400/how-do-i-show-the-site-slogan
+    $lcContent .= \Drupal::config('system.site')->get('name') . "\n";
+    $lcContent .= "</a></div>\n";
+
+    $lcContent .= "<div class='slogan'>\n";
+    // From https://drupal.stackexchange.com/questions/187400/how-do-i-show-the-site-slogan
+    $lcContent .= \Drupal::config('system.site')->get('slogan') . "\n";
+    $lcContent .= "</div>\n";
+
+
+    // From https://drupal.stackexchange.com/questions/199527/how-do-i-correctly-setup-caching-for-my-custom-block-showing-content-depending-o
+    return (array(
+        '#type' => 'markup',
+        '#cache' => array('max-age' => 0),
+        '#markup' => $lcContent,
+    ));
+
+  }
+
+  //-------------------------------------------------------------------------------------------------
+
+}
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
